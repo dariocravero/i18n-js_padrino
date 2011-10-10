585675b937506d542ef16d20b0f5e5d8b0ce9a12
Darío Javier Cravero <dario@qinnova.com.ar>
2011-10-10 16:16:33 +0100

    first commit

---
+++ i18-js_padrino.gemspec
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name = "i18n-js_padrino"
+  s.version = "0.1"
+  s.required_ruby_version = ">= 1.8.6"
+  s.authors = ["Dario Javier Cravero"]
+  s.summary = "i18n-js_padrino is a Padrino's wrapper around i18n-js' Rails plugin for using your I18n locales in JavaScript."
+  s.email = "dario@qinnova.com.ar"
+  s.files = %w{LICENSE README.rdoc Rakefile} + Dir["lib/*.rb"]
+  s.has_rdoc = true
+  s.extra_rdoc_files = ['README.rdoc']
+  s.add_dependency 'i18n-js', '>= 2.0.1'
+  s.homepage = %q{http://github.com/dariocravero/i18n-js_padrino}
+end
---
+++ lib/i18n-js_padrino.rb
@@ -0,0 +1,26 @@
+# Stub Rails so we don't have to change almost everything in obligation_scope
+module SimplesIdeias
+  module I18n
+    module Rails
+      def self.version 
+=begin
+        require 'padrino' unless ::Padrino.respond_to? :version
+        if ::Padrino.version >= "0.10.3"
+          "3.1"
+        else
+          "3"
+        end
+=end
+        # TODO Implement support for sprockets by setting the version to 3.1 if
+        # sprockets is present (perhaps with padrino-sprockets?).
+        # Will also need to define Rails.configuration.assets.enabled 
+        "0"
+      end
+      
+      def self.root
+        Pathname.new(PADRINO_ROOT)
+      end
+    end
+  end
+end
+require "i18n-js"
